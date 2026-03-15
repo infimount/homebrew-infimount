b44579b7792752d3077273c2ba78f19aea95e3c0
roylkng <rajan.kadeval@gmail.com>
2026-03-15 11:06:27 +0530

    chore(formula): bump infimount to 0.2.0

--- Formula/infimount.rb
+++ Formula/infimount.rb
@@ -2,16 +2,16 @@ class Infimount < Formula
   desc "Desktop file and object storage explorer"
   homepage "https://github.com/infimount/infimount"
   license "MIT"
-  version "0.1.0"
+  version "0.2.0"
 
   depends_on :linux
 
-  url "https://github.com/infimount/infimount/releases/download/v0.1.0/Infimount-amd64.deb"
-  sha256 "a907e3c986a6514d4fb138ef07088c345b7f238ef6d532e450de9f3553d8df96"
+  url "https://github.com/infimount/infimount/releases/download/v0.2.0/Infimount-amd64.deb"
+  sha256 "07c34d2bef1b20b59dafb919646c15405629e770d24e37f8fa10c52e9bee5ee4"
 
   def install
     system "ar", "x", "Infimount-amd64.deb"
-    
+
     if File.exist?("data.tar.gz")
       system "tar", "xf", "data.tar.gz"
     elsif File.exist?("data.tar.xz")
