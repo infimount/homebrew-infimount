a9f8c14cff667b52a14fafb6515aa2dafc79d760
roylkng <rajan.kadeval@gmail.com>
2026-03-15 11:06:43 +0530

    chore(cask): bump infimount to 0.2.0

--- Casks/infimount.rb
+++ Casks/infimount.rb
@@ -1,6 +1,6 @@
 cask "infimount" do
-  version "0.1.0"
-  sha256 "72610f728661a199ca1e8f6d8a29bfd71dc5715de9a823a3c1cf289fda41181e"
+  version "0.2.0"
+  sha256 "ad07d8d400f5b6c470afce6e24bd6921049e93ee04ab3633d066fa60bd1e47c7"
 
   url "https://github.com/infimount/infimount/releases/download/v#{version}/Infimount.dmg"
   name "Infimount"
